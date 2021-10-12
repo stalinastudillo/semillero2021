426a1dd19c937738fa4e221cb67d7e5d532b18b8
Stalin <stalin84@hotmail.es>
2021-10-11 23:01:56 -0500

    Se agrega menu de ejemplo

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -2,7 +2,8 @@
     "COMMONS": {
         "MENU": {
             "NMU_KAC_BVDA":"Bienvenida Kleyner Astudillo",
-			"MNU_EJEMPLO":"EJEMPLO"
+			"MNU_EJEMPLO":"EJEMPLO",
+			"MNU_EJEMPLO2":"EJEMPLO 2"
         }
     }
 }
