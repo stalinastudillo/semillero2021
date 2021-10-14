1c07f5497389464bf916daa3629732a1e0551b2d
Luis Yanza <luis.yh1024@gmail.com>
2021-10-14 12:37:27 -0500

    Se agrega Menu de ejemplo 3

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -4,6 +4,7 @@
             "NMU_KAC_BVDA":"Bienvenida Kleyner Astudillo",
 			"MNU_EJEMPLO":"EJEMPLO",
 			"MNU_EJEMPLO2":"EJEMPLO 2",
+			"MNU_EJEMPLO3":"EJEMPLO 3",
 			"MNU_MENU5":"MENU 5",
 			"MNU_MENU6":"MENU 6",
 			"MNU_ECG_BVND":"Bienvenida Erick Cordova"
